daad230d54ef3a49020454fbbed1ae46a55ddfa4
miklik72 <miklik@miklik.cz>
2019-11-03 20:34:35 +0100

    ESP32+ArduinoMEnu Blink buildin led and serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include <Arduino.h>
 
+#include <menu.h>
+#include <menuIO/serialOut.h>
+#include <menuIO/chainStream.h>
+#include <menuIO/serialIn.h>
+
+using namespace Menu;
+
+#define LEDPIN LED_BUILTIN
+#define MAX_DEPTH 1
+
+unsigned int timeOn=10;
+unsigned int timeOff=90;
+
+MENU(mainMenu, "Blink menu", Menu::doNothing, Menu::noEvent, Menu::wrapStyle
+  ,FIELD(timeOn,"On","ms",0,1000,10,1, Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,FIELD(timeOff,"Off","ms",0,10000,10,1,Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,EXIT("<Back")
+);
+
+serialIn serial(Serial);
+MENU_INPUTS(in,&serial);
+
+MENU_OUTPUTS(out,MAX_DEPTH
+  ,SERIAL_OUT(Serial)
+  ,NONE//must have 2 items at least
+);
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
+
 void setup() {
-  // put your setup code here, to run once:
-  // git test
+  pinMode(LEDPIN, OUTPUT);
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Menu 4.x");
+  Serial.println("Use keys + - * /");
+  Serial.println("to control the menu navigation");
+}
+
+bool blink(int timeOn,int timeOff) {
+  return millis()%(unsigned long)(timeOn+timeOff)<(unsigned long)timeOn;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  nav.poll();
+  digitalWrite(LEDPIN, blink(timeOn,timeOff));
 }
